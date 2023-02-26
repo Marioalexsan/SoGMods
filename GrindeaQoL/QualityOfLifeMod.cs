342549ec5f9ae43f8880071bee75ac2a4e772bc7
Marioalexsan <mironalex@hotmail.com>
2023-02-27 01:39:19 +0200

    Added text health display to most health bars

--- GrindeaQoL/QualityOfLifeMod.cs
+++ GrindeaQoL/QualityOfLifeMod.cs
@@ -18,7 +18,7 @@ namespace Marioalexsan.GrindeaQoL
     public class QualityOfLifeMod : Mod
     {
         public override string Name => "Marioalexsan.GrindeaQoL";
-        public override Version Version => new Version(1, 7, 0);
+        public override Version Version => new Version(1, 8, 0);
 
         public static QualityOfLifeMod Instance { get; private set; }
 
